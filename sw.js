9b0b2fc0a33c3296ff9817e556150bad06cc744d
prashantwct <prasarsenal@gmail.com>
2025-12-15 08:21:47 +0000

    PWA

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'pango-v1';
+const ASSETS = [
+  '/',
+  '/static/manifest.json',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+// 1. Install Event: Cache files
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// 2. Fetch Event: Serve from Cache if Offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
